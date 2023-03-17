07e7286ca390888ef25d5f9477740bcb89b2b885
Ken Arnold <arnold@peak.im>
2023-03-17 16:00:05 -0700

    submenus seem to work

--- pynecraft/menus.py
+++ pynecraft/menus.py
@@ -1,29 +1,41 @@
 from __future__ import annotations
 
-import re
-from typing import Callable, Iterable, Tuple, Mapping
+from typing import Callable, Iterable, Tuple
 
 from pynecraft.base import FacingDef, good_facing, Position, Facing, ROTATION_270, DOWN
-from pynecraft.commands import fill, BlockDef, function
+from pynecraft.commands import fill, BlockDef, function, execute, Selector, e
 from pynecraft.function import Function
 from pynecraft.simpler import WallSign
 
 
 class Menu:
-    def __init__(self, tag_base: str, function_factory: Callable[[str], Function], action: Callable[[str], str],
+
+    def __init__(self, home: str | Selector, function_factory: Callable[[str], Function], action: Callable[[str], str],
                  /, wood: BlockDef = 'oak', selected_wood: BlockDef = 'birch', top_blank=True):
-        self._tag_base = tag_base
+        self._home = home if isinstance(home, Selector) else e().tag(home).limit(1)
+        if not self._home.is_single():
+            raise ValueError(f'{home}: Selecting multiple not allowed')
         self._function_factory = function_factory
         self._action = action
         self._wood = wood
         self._selected_wood = selected_wood
         self._top_blank = top_blank
         self._entries = []
-        # noinspection PyArgumentList
-        self.reset = function_factory('reset', home=False)
-        self.init = function_factory('init')
+        self.__functions = {}
+        self._func_prefix = ''
 
-    def add(self, to_add: str | Submenu | Iterable[str | Menu]):
+    def func(self, name):
+        name = self._func_prefix + name
+        try:
+            return self._funcs()[name]
+        except KeyError:
+            func = self._funcs()[name] = self._function_factory(name)
+            return func
+
+    def _funcs(self):
+        return self.__functions
+
+    def add(self, to_add: str | Submenu | Iterable[str | Submenu]):
         if isinstance(to_add, (str, Submenu)):
             self._entries.append(to_add)
         else:
@@ -31,9 +43,8 @@ class Menu:
                 self.add(item)
         return self
 
-    def place(self, pos: Position, facing: FacingDef) -> Mapping[str, Function]:
-        _Placement(self, pos, good_facing(facing)).place()
-        return {'init': self.init, 'reset': self.reset}
+    def place(self, pos: Position, facing: FacingDef) -> None:
+        Placement(self, pos, good_facing(facing)).place()
 
     def end(self, pos: Position, facing: FacingDef) -> Position:
         facing = good_facing(facing)
@@ -46,69 +57,78 @@ class Menu:
             return (0, 0)
         w, h = (len(self._entries), 1)
         for i, e in enumerate(self._entries):
-            if isinstance(e, Menu):
+            if isinstance(e, Submenu):
                 subdim = e._dim()
                 w = max(w, i + subdim[0])
                 h = max(h, 1 + subdim[1])
         return w, h
 
+    def _to_text(self, entry: str):
+        text = tuple(x.strip() for x in entry.split('|'))
+        if self._top_blank and len(text) < 4:
+            text = (None,) + text
+        return text
 
-class _Placement:
+
+class Placement:
     def __init__(self, menu: Menu, pos: Position, facing: Facing):
         self.menu = menu
         self.pos = pos
         self.facing = facing
-        self.place_in = facing.turn(ROTATION_270)
+        self.placing = facing.turn(ROTATION_270)
         self.down = good_facing(DOWN)
 
-        # Just to keep the checker happy, so it knows these exist
-        self.top_blank = None
-        self.entries = None
-        self.action = None
-        self.wood = None
-        self.selected_wood = None
-        self.init = None
-        self.reset = None
-
-        for k, v in menu.__dict__.items():
-            self.__dict__[re.sub('^_', '', k)] = v
-
     def place(self):
-        if not self.entries:
+        if not self.menu._entries:
             return
-        self.reset.add(fill(self.pos, self.menu.end(self.pos, self.facing), 'air'))
-
-        self.init.add(function(self.reset))
-        for i, e in enumerate(self.entries):
-            self.init.add(self.place_sign(i, e))
+        init = self.menu.func('init')
+        init.add(fill(self.pos, self.menu.end(self.pos, self.placing), 'air'))
+        for i, e in enumerate(self.menu._entries):
+            init.add(self.place_sign(i, e))
 
     def place_sign(self, offset, entry):
         if isinstance(entry, Submenu):
-            sign = entry._menu_sign()
+            submenu_pos = self.down.move(self.placing.move(self.pos, offset), 1)
+            entry.place(submenu_pos, self.facing)
+            sign = entry._menu_sign(submenu_pos, self.facing)
         else:
-            text = tuple(x.strip() for x in entry.split('|'))
-            if self.top_blank and len(text) < 4:
-                text = (None,) + text
-            sign = WallSign(text, self.commands(entry), wood=self.wood)
-        yield sign.place(self.place_in.move(self.pos, offset), self.facing)
+            text = self.menu._to_text(entry)
+            sign = WallSign(text, self.commands(entry), wood=self.menu._wood)
+        yield sign.place(self.placing.move(self.pos, offset), self.facing, clear=False)
 
     def commands(self, entry) -> Tuple[str, ...]:
-        return (str(self.action(entry)),)
+        return execute().at(self.menu._home).run(
+            function(self.menu.func('init')),
+            self.menu._action(entry))
 
 
 class Submenu(Menu):
-    def __init__(self, name: str, parent: Menu, function_factory: Callable[[str], Function] = None,
-                 action: Callable[[str], str] = None, /, wood: BlockDef = None, selected_wood: BlockDef = None):
+    def __init__(self, parent: Menu, name: str, function_factory: Callable[[str], Function] = None,
+                 action: Callable[[str], str] = None, /, text: str = None, wood: BlockDef = None,
+                 selected_wood: BlockDef = None):
+        self.parent = parent
         if not function_factory:
             function_factory = parent._function_factory
         if not action:
             action = parent._action
+        if not text:
+            text = parent._to_text(name.title())
+        self.text = text
         if not wood:
             wood = parent._wood
         if not selected_wood:
-            wood = parent._selected_wood
-        super().__init__(parent._tag_base, function_factory, action, wood, selected_wood)
+            selected_wood = parent._selected_wood
+        super().__init__(parent._home, function_factory, action, wood, selected_wood)
+        name_prefix = name.replace(' ', '_').lower()
+        if parent._func_prefix:
+            self._func_prefix = f'{parent._func_prefix}_{name_prefix}_'
+        else:
+            self._func_prefix = f'{name_prefix}_'
         self._name = name
 
-    def _menu_sign(self):
-        return WallSign((None, self._name))
+    def _funcs(self):
+        return self.parent._funcs()
+
+    def _menu_sign(self, pos, facing):
+        return WallSign(self.text, (
+            execute().at(self._home).run(function(self.func('init')), )))
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,41 +1,48 @@
 import unittest
 
 from pynecraft.base import r, NORTH, SOUTH, EAST, WEST
-from restworld.menus import Menu, Submenu
+from pynecraft.function import Function
+from pynecraft.menus import Menu, Submenu
 
-dummy = lambda x: x
+
+def func(x, **kwargs):
+    return Function(x, **kwargs)
+
+
+def action(x):
+    return x
 
 
 class TestMenu(unittest.TestCase):
     def test_empty_size(self):
-        menu = Menu('base', dummy, dummy)
+        menu = Menu('base', func, action)
         self.assertEqual((0, 0), menu._dim())
 
     def test_one_size(self):
-        menu = Menu('base', dummy, dummy)
+        menu = Menu('base', func, action)
         menu.add('One')
         self.assertEqual((1, 1), menu._dim())
 
     def test_nosubs_size(self):
-        menu = Menu('base', dummy, dummy)
+        menu = Menu('base', func, action)
         menu.add(('One', 'Two', 'Three'))
         self.assertEqual((3, 1), menu._dim())
 
     def test_basic_sub_size(self):
-        menu = Menu('base', dummy, dummy)
-        submenu = Submenu('Sub', menu).add(('1', '2', '3'))
+        menu = Menu('base', func, action)
+        submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add(submenu)
         self.assertEqual((3, 2), menu._dim())
 
     def test_second_sub_size(self):
-        menu = Menu('base', dummy, dummy)
-        submenu = Submenu('Sub', menu).add(('1', '2', '3'))
+        menu = Menu('base', func, action)
+        submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add('empty').add(submenu)
         self.assertEqual((4, 2), menu._dim())
 
     def test_second_area(self):
-        menu = Menu('base', dummy, dummy)
-        submenu = Submenu('Sub', menu).add(('1', '2', '3'))
+        menu = Menu('base', func, action)
+        submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add('empty').add(submenu)
         pos = r(1, 2, 3)
         self.assertEquals(r(1, 1, 0), menu.end(pos, NORTH))
