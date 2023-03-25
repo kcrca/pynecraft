d851a3d3756ceacf3d017526c0540f58e027052b
Ken Arnold <arnold@peak.im>
2023-03-25 10:37:46 -0700

    update tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -15,33 +15,33 @@ def action(x):
 
 class TestMenu(unittest.TestCase):
     def test_empty_size(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         self.assertEqual((0, 0), menu._dim())
 
     def test_one_size(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         menu.add('One')
         self.assertEqual((1, 1), menu._dim())
 
     def test_nosubs_size(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         menu.add(('One', 'Two', 'Three'))
         self.assertEqual((3, 1), menu._dim())
 
     def test_basic_sub_size(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add(submenu)
         self.assertEqual((3, 2), menu._dim())
 
     def test_second_sub_size(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add('empty').add(submenu)
         self.assertEqual((4, 2), menu._dim())
 
     def test_second_area(self):
-        menu = Menu('base', func, action)
+        menu = Menu(func, action)
         submenu = Submenu(menu, 'Sub').add(('1', '2', '3'))
         menu.add('empty').add(submenu)
         pos = r(1, 2, 3)
