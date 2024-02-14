0a84e0f9b27092376f46def9709695aa4fae3d3d
Ken Arnold <arnold@peak.im>
2024-02-14 14:22:15 -0800

    bump to 90.7.1 (just to get README shown on pypi)

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022-24, Ken Arnold'
 author = 'Ken Arnold'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7'
+release = '0.7.1'
 
 # -- General configuration ---------------------------------------------------
 
