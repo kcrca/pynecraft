82c3c6f7925f87d1a0237160fdebfcc3bd8f47e9
Ken Arnold <arnold@peak.im>
2024-02-13 16:15:16 -0800

    first website

---
+++ site/sync_site.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+cd `dirname $0`
+./build.sh
+./favicon.sh
+dst_dir="pynecraft"
+rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.nyc1.nearlyfreespeech.net:$dst_dir/
