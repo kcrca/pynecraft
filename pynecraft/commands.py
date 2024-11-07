83d0519c42b12f5b6656a952dc11faf52ac673e3
Ken Arnold <arnold@peak.im>
2024-11-06 17:09:42 -0800

    Revert "Revert "Force return arg to be int if its bool": It broke things"
    This reverts commit aa271a242038858710d2ebd0e65e7e7885f3183e.

--- pynecraft/commands.py
+++ pynecraft/commands.py
@@ -2886,7 +2886,8 @@ def return_(value: int = None) -> _ReturnMod | str:
     cmd._add('$return')
     if value is None:
         return cmd._start(_ReturnMod())
-    cmd._add(value)
+    # False is an int, apparently, so we force it to be an actual int
+    cmd._add(int(value))
     return str(cmd)
 
 
